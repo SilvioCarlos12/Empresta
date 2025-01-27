805fba836b3988443a8831370ef7724e6b50b071
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-01-27 18:14:21 -0300

    test: adicionado teste para adicao de cliente e criacao de funcionario

--- Empresta.Dominio.Teste/FuncionarioTeste.cs
+++ Empresta.Dominio.Teste/FuncionarioTeste.cs
@@ -29,5 +29,32 @@ namespace Empresta.Dominio.Teste
             Assert.Equal(funcionario.Telefone, telefone);
             Assert.Equal(funcionario.Endereco, endereco);
         }
+
+        [Fact(DisplayName = "Funcinario deve adicionar um cliente")]
+        public void DeveAdicionarUmClienteUmFuncionario()
+        {
+            //Arrange
+            var nome = "Teste";
+            var telefone = new TelefoneBuild().Build();
+            var endereco = new EnderecoBuild().Build();
+            var cliente = new ClienteBuild().Build();
+
+            //Act
+
+            var funcionario = new FuncionarioBuild()
+            .ComNome(nome)
+            .ComEndereco(endereco)
+            .ComTelefone(telefone)
+            .Build();
+
+            funcionario.AdicionarCliente(cliente);
+
+            //Assert
+
+            Assert.Equal(funcionario.Nome, nome);
+            Assert.Equal(funcionario.Telefone, telefone);
+            Assert.Equal(funcionario.Endereco, endereco);
+            Assert.Equal(funcionario.Clientes.First(), cliente);
+        }
     }
 }
