528c6773bb0447b390cc04000321e887c5cdb67c
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-01-27 15:09:39 -0300

    feat: adicionado classe de pessoa e cliente

---
+++ Empresta.Dominio/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using Empresta.Dominio.Vo;
+
+namespace Empresta.Dominio
+{
+    public abstract class Pessoa
+    {
+        protected Pessoa(string nome, Telefone telefone, Endereco endereco)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+        }
+
+        public string Nome { get; set; }
+        public Telefone  Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
