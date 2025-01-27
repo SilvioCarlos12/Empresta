c3362a03fae45c85e715339ef0ae0b3c7ba61fef
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-01-27 15:06:13 -0300

    feat: adicionado Vo de telefone e Endereco

---
+++ Empresta.Dominio/Vo/Endereco.cs
@@ -0,0 +1,22 @@
+﻿namespace Empresta.Dominio.Vo
+{
+    public record Endereco
+    {
+        public Endereco(string rua, string bairro, string cidade, string cep, string estado, string numero)
+        {
+            Rua = rua;
+            Bairro = bairro;
+            Cidade = cidade;
+            Cep = cep;
+            Estado = estado;
+            Numero = numero;
+        }
+
+        public string Rua { get; private set; } = string.Empty;
+        public string Bairro { get; private set; } = string.Empty;
+        public string Cidade { get; private set; } = string.Empty;
+        public string Cep { get; private set; } = string.Empty;
+        public string Estado { get; private set; } = string.Empty;
+        public string Numero { get; private set; } = string.Empty;
+    }
+}
---
+++ Empresta.Dominio/Vo/Telefone.cs
@@ -0,0 +1,13 @@
+﻿namespace Empresta.Dominio.Vo
+{
+    public record Telefone
+    {
+        public string Dd {  get; private set; } = string.Empty;
+        public string Numero { get; private set; } = string.Empty;
+        public Telefone(string dd,string numero) { 
+          
+            Dd = dd;
+            Numero = numero;
+        }
+    }
+}
