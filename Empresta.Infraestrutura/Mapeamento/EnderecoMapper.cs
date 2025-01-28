c2baa68f257eb5c8b94f59e10bf1d3afe9b6b4f9
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-01-28 14:04:07 -0300

    feat:adicionado mapper

---
+++ Empresta.Infraestrutura/Mapeamento/EnderecoMapper.cs
@@ -0,0 +1,18 @@
+﻿using Empresta.Dominio.Vo;
+using MongoDB.Bson.Serialization;
+
+namespace Empresta.Infraestrutura.Mapeamento;
+
+public static class EnderecoMapper
+{
+    public static void Register()
+    {
+        if (!BsonClassMap.IsClassMapRegistered(typeof(Endereco)))
+        {
+            BsonClassMap.RegisterClassMap<Endereco>(cm =>
+            {
+                cm.AutoMap();
+            });
+        }
+    }
+}
---
+++ Empresta.Infraestrutura/Mapeamento/FuncionarioMapper.cs
@@ -0,0 +1,21 @@
+﻿using Empresta.Dominio;
+using MongoDB.Bson.Serialization;
+
+namespace Empresta.Infraestrutura.Mapeamento;
+
+public static class FuncionarioMapper
+{
+    public static void Register()
+    {
+        if (!BsonClassMap.IsClassMapRegistered(typeof(Funcionario)))
+        {
+            BsonClassMap.RegisterClassMap<Funcionario>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+                cm.MapMember(p => p.Clientes).SetElementName("Clientes");
+                cm.MapMember(p => p.Id).SetElementName("FuncionarioId");
+            });
+        }
+    }
+}
