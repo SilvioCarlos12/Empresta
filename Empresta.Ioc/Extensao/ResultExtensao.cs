2194020e1511c03d2935d9fca49f4ac4fd01ce2b
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-03 13:35:41 -0300

    feat: adicionado metodo de extensão

---
+++ Empresta.Ioc/Extensao/ResultExtensao.cs
@@ -0,0 +1,13 @@
+﻿using Empresta.Aplicacao.Dto;
+using Microsoft.AspNetCore.Http;
+
+namespace Empresta.Ioc.Extensao
+{
+    public static class ResultExtensao
+    {
+        public static IResult ToResultPromblem(this  ErroDto result)
+        {
+            return Results.Problem(statusCode: int.Parse(result.CodigoErro), detail: result.Mensagem);
+        }
+    }
+}
