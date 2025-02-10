2dde934883196c42d5f70ca2a047b7ce50642cc2
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-10 13:20:32 -0300

    feat: adicionado build de EnderecoDtoBuild.cs e TelefoneDtoBuild.cs

---
+++ Empresta.Aplicacao.Teste/Builds/EnderecoDtoBuild.cs
@@ -0,0 +1,65 @@
+﻿using Empresta.Aplicacao.Dto;
+
+namespace Empresta.Aplicacao.Teste.Builds;
+
+public sealed class EnderecoDtoBuild
+{
+    private string _rua;
+    private string _bairro;
+    private string _cidade;
+    private string _cep;
+    private string _estado;
+    private string _numero;
+
+    public EnderecoDtoBuild()
+    {
+        _rua = "Rua 12";
+        _bairro = "Bairro 13";
+        _cidade = "Cidade 14";
+        _cep = "65040430";
+        _estado = "Estado 15";
+        _numero = "123";
+    }
+    
+    public EnderecoDtoBuild ComCep(string cep)
+    {
+        _cep = cep;
+        return this;
+    }
+
+    public EnderecoDtoBuild ComCidade(string cidade)
+    {
+        _cidade = cidade;
+        return this;
+    }
+
+    public EnderecoDtoBuild ComRua(string rua)
+    {
+        _rua = rua;
+        return this;
+    }
+
+    public EnderecoDtoBuild ComNumero(string numero)
+    {
+        _numero = numero;
+        return this;
+    }
+
+    public EnderecoDtoBuild ComEstado(string estado)
+    {
+        _estado = estado;
+        return this;
+    }
+    
+    public EnderecoDtoBuild ComBairro(string bairro)
+    {
+        _bairro = bairro;
+        return this;
+    }
+
+    public EnderecoDto Build()
+    {
+        return new EnderecoDto(_rua,_bairro,_cidade,_cep,_estado,_numero);
+    }
+
+}
---
+++ Empresta.Aplicacao.Teste/Builds/TelefoneDtoBuild.cs
@@ -0,0 +1,32 @@
+﻿using Empresta.Aplicacao.Dto;
+
+namespace Empresta.Aplicacao.Teste.Builds;
+
+public sealed class TelefoneDtoBuild
+{
+    private string _dd;
+    private string _numeroTelefone;
+
+    public TelefoneDtoBuild()
+    {
+        _dd = "098";
+        _numeroTelefone = "987246527";
+    }
+
+    public TelefoneDtoBuild ComDd(string dd)
+    {
+        _dd = dd;
+        return this;
+    }
+
+    public TelefoneDtoBuild ComNumeroTelefone(string telefone)
+    {
+        _numeroTelefone = telefone;
+        return this;
+    }
+
+    public TelefoneDto Buid()
+    {
+        return new TelefoneDto(_dd, _numeroTelefone);
+    }
+}
