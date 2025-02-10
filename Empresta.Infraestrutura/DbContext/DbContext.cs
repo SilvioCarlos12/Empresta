845597ff03f3a09bbcbb148a13b349bf329916cf
silvio carlos <silvio.carlos@toutsolucoes.com.br>
2025-02-10 13:20:00 -0300

    refact: remoção de comentario, adição de validação de nulo e adicionado validação de Guid

--- Empresta.Infraestrutura/DbContext/DbContext.cs
+++ Empresta.Infraestrutura/DbContext/DbContext.cs
@@ -5,19 +5,17 @@ using MongoDB.Driver;
 
 namespace Empresta.Infraestrutura.DbContext
 {
-
     public sealed class DbContext : IDbContext
     {
         private readonly IMongoDatabase _db;
 
         public DbContext(IOptions<MongoDbSettings> options)
         {
-            var clent = new MongoClient(options.Value.ConnectionString);
-            _db = clent.GetDatabase(options.Value.Database);
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+            _db = mongoClient.GetDatabase(options.Value.Database);
 
-            // Check if the database connection is successful
             var pingTask = _db.RunCommandAsync((Command<BsonDocument>)"{ping:1}");
-            if (!pingTask.Wait(TimeSpan.FromSeconds(5))) // Adjust timeout as needed
+            if (!pingTask.Wait(TimeSpan.FromSeconds(5)))
             {
                 throw new Exception("Database connection timed out.");
             }
@@ -26,6 +24,7 @@ namespace Empresta.Infraestrutura.DbContext
                 throw new Exception("Failed to connect to the database.");
             }
         }
+
         public IMongoCollection<T> GetCollection<T>()
         {
             Type targetType = typeof(T);
@@ -33,7 +32,6 @@ namespace Empresta.Infraestrutura.DbContext
             return _db.GetCollection<T>(targetType.Name);
         }
 
-        // Common method for inserting document
         public async Task<T> InsertDocument<T>(T payload, CancellationToken cancellationToken)
         {
             await GetCollection<T>().InsertOneAsync(payload, null, cancellationToken);
@@ -41,24 +39,22 @@ namespace Empresta.Infraestrutura.DbContext
             return payload;
         }
 
-        //Common method for updating the document
         public async Task<T> UpdateDocument<T>(T payload, CancellationToken cancellationToken)
         {
             var filter = Builders<T>.Filter.Eq("_id",
-                Guid.Parse(payload?.GetType()?.GetProperty("Id")?.GetValue(payload)?.ToString()));
+                Guid.Parse(payload?.GetType()?.GetProperty("Id")?.GetValue(payload)?.ToString() ?? string.Empty));
 
-            var updateResult = await GetCollection<T>().ReplaceOneAsync(filter, payload, new ReplaceOptions(), cancellationToken);
+            await GetCollection<T>().ReplaceOneAsync(filter, payload, new ReplaceOptions(), cancellationToken);
 
             return payload;
         }
 
-        //Common method for delete the document
         public async Task<T> DeleteDocument<T>(T payload, CancellationToken cancellationToken)
         {
             var filter = Builders<T>.Filter.Eq("_id",
-                ObjectId.Parse(payload?.GetType()?.GetProperty("Id")?.GetValue(payload)?.ToString()));
+                Guid.Parse(payload?.GetType()?.GetProperty("Id")?.GetValue(payload)?.ToString() ?? string.Empty));
 
-            var deleteResult = await GetCollection<T>().DeleteOneAsync(filter, cancellationToken);
+            await GetCollection<T>().DeleteOneAsync(filter, cancellationToken);
 
             return payload;
         }
